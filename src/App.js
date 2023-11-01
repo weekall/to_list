05bcf2d13ed821cb6ff9adfabfa49c3dc3798813
weekall <109850481+weekall@users.noreply.github.com>
2023-11-01 17:53:48 +0300

    changing color-priority

--- src/App.js
+++ src/App.js
@@ -4,27 +4,10 @@ import List from "./components/list/List";
 import Navigation from "./components/navigation/Navigation";
 
 export default function App() {
-  const [list, setList] = useState([
-    {
-      title: "walk with dog",
-      description: "walk with dog",
-      priority: "1"
-    },
-    {
-      title: "walk with dog",
-      description: "walk with dog",
-      priority: "2"
-    },
-    {
-      title: "walk with dog",
-      description: "walk with dog",
-      priority: "3"
-    }
-  ]);
+  
   return (
     <React.Fragment>
-      <Navigation data={list} />
-      
+      <List />
     </React.Fragment>
   );
 }
--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,16 +1,58 @@
 import "./List.css";
-function List({ data }) {
+import { useState } from "react";
+function List() {
+  const [list, setList] = useState([
+    {
+      title: "walk with dog",
+      description: "walk with dog",
+      priority: 1
+    },
+    {
+      title: "go for the milk",
+      description: "go for the milk",
+      priority: 2
+    },
+    {
+      title: "buy new clothes",
+      description: "buy new clothes",
+      priority: 3
+    }
+  ]);
+  function handlePriorityDisplay(priorityNumber){
+    switch(priorityNumber){
+      case 1:
+        return "high-priority";
+      case 2:
+        return "medium-priority";
+      case 3:
+        return "low-priority";
+    }
+  }
     return (
       <div>
-        <ul>
-          {data.map(item => (
-            <>
-              <li>{item.title}</li> 
-              <li>{item.description}</li> 
-              <li>{item.priority}</li>
-            </>
+        <label class="create">Create</label>
+        <form action="" method="">
+            <div class="form-container">
+                <label>title</label>
+                <textarea type="text" name="title"></textarea>
+            </div> 
+            <div class="form-container">
+                <label>description</label>
+                <textarea type="text" name="description"></textarea>
+            </div>
+            <div class="form-container">
+                <label>priority</label>
+                <input name="prioroty" type="number"></input>
+            </div>
+        </form>
+        <div className="tasks-list">
+          {list.map(item => (
+            <div className={`list-item ${handlePriorityDisplay(item.priority)}`}>
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
     );
   }
